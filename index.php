646dc5cce4a4747c34466086524ef58e21483469
Matthew Wilber <matt@Matthews-Mac-mini.local>
2012-02-17 22:18:03 -0500

    - Add google analytics

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 <script src="js/fb.js"></script>
 <script type="text/javascript" src="js/libs/jquery.facebook.multifriend.select.js"></script>
 <script>
-	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+	var _gaq=[['_setAccount','UA-76054-11'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
 	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 	s.parentNode.insertBefore(g,s)}(document,'script'));
--- tab.php
+++ tab.php
@@ -99,7 +99,7 @@
 	</div> <!--! end of #container -->
 <script>
 
-	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+	var _gaq=[['_setAccount','UA-76054-11'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
 
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
 
