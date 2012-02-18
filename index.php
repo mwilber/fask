e044c1c9e058e8d02cb78022bb6c3fad83c67a60
Matthew Wilber <matt@Matthews-Mac-mini.local>
2012-02-17 21:51:05 -0500

    update layout for demo app

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 				method: 'popup',									// 'redirect' or 'popup'
 				target: 'https://apps.facebook.com/appstarterkit/'	// endpoint url if loginmethod is 'redirect'
 			},
-			likegate:{
+			likegate:{												// Not required for the tab app based like gate
 				targetid: '130551640347075',
 				targeturl: 'http://www.facebook.com/apps/application.php?id=130551640347075'
 			}
@@ -48,26 +48,33 @@
 <body>
 
 <div id="container">
+	<header></header>
 	<div id="main" role="main">
 		<!--! begin app content -->
-		<strong>Basic Features</strong>
+		<strong>Created by <a href="http://www.facebook.com/greenzeta?sk=app_309846011404">Internet Software Developer Matthew Wilber</a></strong>
+		<p>
+			FASK provides a ready made starting point for building facebook apps using the facebook javascript api.
+			It&rsquo;s built on the <a href="http://html5boilerplate.com/" target="_blank">HTML5 Boilerplate</a> and provides functions for many of the most commonly used facebook functions.
+		</p>
+		<a id="download" href="fask.zip"><img src="images/download.png"><br/>Download FASK 2.0.1</a>
+		<h3>Basic Features</h3>
 		<ul>
 			<li><a href="#" onclick="WallPost('https://fask.herokuapp.com/', 'FacebookAppStarter', 'All the facebook basics', 'https://fask.herokuapp.com/images/fask.png', 'FASK'); return false;">Wall Post</a></li>
 			<li><a href="#" onclick="ShareMessage('https://fask.herokuapp.com/', 'FacebookAppStarter', 'All the facebook basics', 'https://fask.herokuapp.com/images/fask.png'); return false;">Private Message</a></li>
 			<li><a href="#" onclick="SendInvite('All the facebook basics'); return false;">App Invite</a></li>
 			<li><a href="#" onclick="AddToPage(); return false;">Add This App To Page Tab</a></li>
 		</ul>
-		<strong>Login / Authorization</strong>
+		<h3>Login / Authorization</h3>
 		<ul>
 			<li><a href="#" onclick="Login(); return false;">Login</a></li>
 			<li><a href="#" onclick="Logout(); return false;">Logout</a></li>
 		</ul>
-		<strong>Advanced Features (extended permissions required)</strong>
+		<h3>Advanced Features (extended permissions required)</h3>
 		<ul>
 			<li><a href="#" onclick="FriendPost('https://fask.herokuapp.com/', 'FacebookAppStarter', 'All the facebook basics. Sent to your Friends!', 'https://fask.herokuapp.com/images/fask.png', 'FASK'); return false;">Post to Friend's Wall</a> (read_stream,publish_stream)</li>
 			<li><a href="#" onclick="LikeGate(); return false;">Like Gate</a> (read_stream) <div class="fb-like" data-href="http://www.facebook.com/apps/application.php?id=130551640347075" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></li>
 		</ul>
-		<strong>Other</strong>
+		<h3>Other</h3>
 		<ul>
 			<li>Bookmarklet -><a href="javascript:var appid=''; appid=prompt('Enter App ID:'); window.open('http://www.facebook.com/dialog/pagetab?app_id='+appid+'&next=http://www.facebook.com');">Add to Page</a></li>
 		</ul>
--- tab.php
+++ tab.php
@@ -1,6 +1,5 @@
 <?php
 	$likesPage = false;
-	$signed_request = $_REQUEST['signed_request'];
 
 	function parsePageSignedRequest() {
 		if (isset($_REQUEST['signed_request'])) {
@@ -52,15 +51,52 @@
 	<script src="js/libs/modernizr-2.0.6.min.js"></script>
 </head>
 <body>
-<?php if($likesPage): ?>
-	<div id="main" class="wrapper like" style="">
-		<a id="get_started" href="https://apps.facebook.com/appstarterkit/" target="_blank">Go To The Main Page</a>
-	</div>
-<?php else: ?>
-	<div class="wrapper nolike" style="">
-		Click the Like button above to continue.
-	</div>
-<?php endif; ?>
+	<div id="container">
+		<header></header>
+		<div id="main" role="main">
+			<!--! begin app content -->
+			<strong>Created by <a href="http://www.facebook.com/greenzeta?sk=app_309846011404">Internet Software Developer Matthew Wilber</a></strong>
+			<p>
+				FASK provides a ready made starting point for building facebook apps using the facebook javascript api.
+				It&rsquo;s built on the <a href="http://html5boilerplate.com/" target="_blank">HTML5 Boilerplate</a> and provides functions for many of the most commonly used facebook functions.
+			</p>
+			<?php if(!$likesPage): ?>
+			<strong>Click the "like" button above to explore the features.</strong>
+			<?php endif; ?>
+			<a id="download" href="fask.zip"><img src="images/download.png"><br/>Download FASK 2.0</a>
+			<?php if($likesPage): ?>
+			<h3>Basic Features</h3>
+			<ul>
+				<li><a href="#" onclick="WallPost('https://fask.herokuapp.com/', 'FacebookAppStarter', 'All the facebook basics', 'https://fask.herokuapp.com/images/fask.png', 'FASK'); return false;">Wall Post</a></li>
+				<li><a href="#" onclick="ShareMessage('https://fask.herokuapp.com/', 'FacebookAppStarter', 'All the facebook basics', 'https://fask.herokuapp.com/images/fask.png'); return false;">Private Message</a></li>
+				<li><a href="#" onclick="SendInvite('All the facebook basics'); return false;">App Invite</a></li>
+				<li><a href="#" onclick="AddToPage(); return false;">Add This App To Page Tab</a></li>
+			</ul>
+			<h3>Login / Authorization</h3>
+			<ul>
+				<li><a href="#" onclick="Login(); return false;">Login</a></li>
+				<li><a href="#" onclick="Logout(); return false;">Logout</a></li>
+			</ul>
+			<h3>Advanced Features (extended permissions required)</h3>
+			<ul>
+				<li><a href="#" onclick="FriendPost('https://fask.herokuapp.com/', 'FacebookAppStarter', 'All the facebook basics. Sent to your Friends!', 'https://fask.herokuapp.com/images/fask.png', 'FASK'); return false;">Post to Friend's Wall</a> (read_stream,publish_stream)</li>
+				<li><a href="#" onclick="LikeGate(); return false;">Like Gate</a> (read_stream) <div class="fb-like" data-href="http://www.facebook.com/apps/application.php?id=130551640347075" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></li>
+			</ul>
+			<h3>Other</h3>
+			<ul>
+				<li>Bookmarklet -><a href="javascript:var appid=''; appid=prompt('Enter App ID:'); window.open('http://www.facebook.com/dialog/pagetab?app_id='+appid+'&next=http://www.facebook.com');">Add to Page</a></li>
+			</ul>
+			<?php endif; ?>
+			<!--! end app content -->
+		</div>
+		<div id="fb-root"></div>
+		<div id="jfmfs-dialog">
+			<a href="#" class="button" onclick="$(this).parent().hide(); return false;">Close</a>
+			<label>Message:</label><textarea id="jfmfs-message"></textarea>
+			<div id="jfmfs-container"></div>
+			<button id="jfmfs-post" class="button">POST</button>
+		</div>
+	</div> <!--! end of #container -->
 <script>
 
 	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
